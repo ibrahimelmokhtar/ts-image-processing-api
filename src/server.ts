f1349d7e1c47eda4c07883ebb655e78b68ea4099
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2022-04-19 13:59:11 +0200

    Seperate server configuration from server code

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,4 @@
-import express, { Application, Request, Response } from 'express';
-import mainRoute from './routes';
-
-
-// set port and host values:
-const port: string = (process.env.PORT || '5000');
-const host: string = 'http://127.0.0.1';
-
-// create an express application:
-const app: Application = express();
-
-// express app will use json format:
-app.use(express.json());
-
-// express app will use main route for '/api' route:
-app.use('/api', mainRoute);
+import { app, port, host } from './server/config';
 
 // express app will listen to specific port:
 app.listen(port, (): void => {
---
+++ src/server/config.ts
@@ -0,0 +1,22 @@
+import express, { Application } from 'express';
+import mainRoute from '../routes';
+
+
+// set port and host values:
+const port: string = (process.env.PORT || '5000');
+const host: string = 'http://127.0.0.1';
+
+// create an express application:
+const app: Application = express();
+
+// express app will use json format:
+app.use(express.json());
+
+// express app will use main route for '/api' route:
+app.use('/api', mainRoute);
+
+export {
+    app,
+    port,
+    host,
+};
